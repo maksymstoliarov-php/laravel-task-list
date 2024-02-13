544d3770b3cd55f5c9832a6fbab833c3db5341a3
Maksym Stoliarov <Windmill78max@gmail.com>
2024-02-13 12:31:42 +0100

    Tailwind form classes

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,51 +2,43 @@
 
 @section('title', isset($task) ? 'Update task' : "Create task")
 
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
     <form action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}" method="POST">
         @csrf
         @method(isset($task) ? 'PUT' : 'POST')
-        <div>
+        <div class="mb-4">
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title') ?? (isset($task) ? $task->title : '') }}">
+            <input type="text" name="title" id="title" @class(['border-red-500' => $errors->has('title')]) value="{{ old('title') ?? (isset($task) ? $task->title : '') }}">
             @error('title')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="description">Description</label>
-            <input type="text" name="description" id="description" value="{{ old('description') ?? (isset($task) ? $task->description : '') }}">
+            <input type="text" name="description" id="description" @class(['border-red-500' => $errors->has('description')]) value="{{ old('description') ?? (isset($task) ? $task->description : '') }}">
             @error('description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="long_description">Long description</label>
-            <textarea name="long_description" id="long_description" cols="30" rows="10">{{ old('long_description') ?? (isset($task) ? $task->long_description : '') }}</textarea>
+            <textarea name="long_description" id="long_description" @class(['border-red-500' => $errors->has('long_description')]) cols="30" rows="10">{{ old('long_description') ?? (isset($task) ? $task->long_description : '') }}</textarea>
             @error('long_description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="completed">Completed</label>
             <input type="checkbox" name="completed" id="completed" value="1" {{ old('completed') || (isset($task) && $task->completed) ? 'checked' : '' }}>
             @error('completed')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
-            <button type="submit">
+        <div class="flex gap-2">
+            <button type="submit" class="btn">
                 {{ isset($task) ? 'Update' : 'Create' }}
             </button>
+            <a href="{{ @route($task->id ? 'tasks.show' : 'tasks.index', $task->id ? ['task' => $task->id] : []) }}" class="btn">Cancel</a>
         </div>
     </form>
 @endsection
